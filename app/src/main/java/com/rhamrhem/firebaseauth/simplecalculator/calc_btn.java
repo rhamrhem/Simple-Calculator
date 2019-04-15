5614db65f9b19f8199d00bd2b3b4e587d3c9bead
rhamrhem <rhamrhemjaafar@gmail.com>
2019-04-15 14:46:29 +0800

    No Back-end yet

---
+++ app/src/main/java/com/rhamrhem/firebaseauth/simplecalculator/calc_btn.java
@@ -0,0 +1,13 @@
+package com.rhamrhem.firebaseauth.simplecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class calc_btn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc_btn);
+    }
+}
